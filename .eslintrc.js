94be7af0f667435786ba5586eda3c1230f9cae7a
Daniel Puscher <dev@daniel-puscher.de>
2019-02-03 18:16:40 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true,
+  },
+  "extends": ["eslint:recommended", "plugin:react/recommended", "airbnb"],
+  "plugins": ["react"],
+  "rules": {
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "no-unused-vars": ["error", { "args": "none" }],
+    "import/no-extraneous-dependencies": ["error", { "devDependencies": true }],
+    "jsx-a11y/anchor-is-valid": ["error", {
+        "components": ["Link"],
+        "specialLink": ["route"],
+        "aspects": ["invalidHref", "preferButton"],
+    }],
+    "import/extensions": "off",
+    "import/no-unresolved": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-one-expression-per-line": "off",
+  }
+};
