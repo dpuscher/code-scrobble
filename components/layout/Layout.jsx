84ab42af0cea8dd2fe4d14b99ecdc48b8279d003
Daniel Puscher <dev@daniel-puscher.de>
2019-02-18 01:55:53 +0100

    Add Favicon

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -11,6 +11,13 @@ const Layout = ({ children, title }) => (
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
       <title>{title}</title>
+      <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
+      <link rel="manifest" href="/static/site.webmanifest" />
+      <link rel="shortcut icon" href="/static/favicon.ico" />
+      <meta name="msapplication-TileColor" content="#ffc40d" />
+      <meta name="msapplication-config" content="/static/browserconfig.xml" />
     </Head>
 
     {children}
