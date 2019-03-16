4f2f3335299c036a00b85dd5cee162c043df7469
Daniel Puscher <dev@daniel-puscher.de>
2019-03-16 01:32:00 +0100

    Improve behaviour of back button

--- components/BackButton.jsx
+++ components/BackButton.jsx
@@ -3,15 +3,29 @@ import { IoIosArrowBack } from 'react-icons/io';
 import { silver } from '../lib/colors';
 import { Button } from './styles/BackButton.styles';
 
-const BackButton = () => {
-  const handleClick = () => Router.back();
+const getHostFromUrl = url => (
+  (/\/\/([^/]+)\//i.exec(url) || [])[1]
+);
 
-  return (
-    <Button onClick={handleClick}>
-      <IoIosArrowBack css="margin-right: 7px" color={silver} size="25px" />
-      Back
-    </Button>
-  );
+const hasExternalReferrer = () => {
+  if (!document.referrer) return true;
+  return getHostFromUrl(document.referrer) !== getHostFromUrl(window.location.href);
 };
+
+const handleClick = () => {
+  if (hasExternalReferrer()) {
+    Router.push('/');
+  } else {
+    Router.back();
+  }
+};
+
+
+const BackButton = () => (
+  <Button onClick={handleClick}>
+    <IoIosArrowBack css="margin-right: 7px" color={silver} size="25px" />
+    Back
+  </Button>
+);
 
 export default BackButton;
