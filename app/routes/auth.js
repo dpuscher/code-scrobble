db4ee4fe68382dc82a8ebd24004df712fab67349
Daniel Puscher <dev@daniel-puscher.de>
2020-01-23 23:58:04 +0100

    Dynamic callback url for lastfm auth

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,7 +1,11 @@
 const passport = require('passport');
 const Router = require('express').Router();
 
-Router.get('/lastfm', passport.authenticate('lastfm'));
+Router.get('/lastfm', function (req, res, next) {
+  passport.authenticate('lastfm', {
+    callbackURL: `${req.protocol}://${req.headers.host}/auth/lastfm/callback`,
+  })(req, res, next)
+})
 
 Router.get(
   '/lastfm/callback',
