a1dfdfcf7b5dd747030ab1f2d0b50c8ef37c039e
Daniel Puscher <dev@daniel-puscher.de>
2026-02-18 22:05:00 +0100

    build: update dockerfile and modernize dependencies

--- vandelay-js.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  includePaths: [__dirname],
-  excludePatterns: [
-    '**/*.spec.*',
-    '**/__mocks__/*',
-  ],
-  importGroups: ['react', 'react-dom', 'redux', 'react-redux', 'prop-types'],
-};
