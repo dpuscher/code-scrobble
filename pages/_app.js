8ccd9ac856b9615e0c30398108bb484129e78571
Daniel Puscher <dev@daniel-puscher.de>
2026-02-19 14:04:37 +0100

    feat: update React ecosystem 16→18
    - react/react-dom 16→18, react-test-renderer 16→18
    - react-redux 7→9
    - next-redux-wrapper 4→8: convert _app.js from class component HOC to
      function component using useWrappedStore hook; export wrapper from
      client/reduxStore.js via createWrapper()
    - styled-components 5→6: add babel-plugin-styled-components as explicit
      devDependency (no longer bundled in v6)
    - redux 4→5, redux-thunk 2→3: named export (thunk vs default export);
      update import in reduxStore.js and all test files
    - Remove enzyme + enzyme-adapter-react-16 (not compatible with React 18);
      replace with @testing-library/react; clean up setupTests.js
    - Add @testing-library/dom peer dependency

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,26 @@
-import App from 'next/app';
-import Head from 'next/head';
 import React from 'react';
-import withRedux from 'next-redux-wrapper';
+import Head from 'next/head';
 import { Provider } from 'react-redux';
 import BaseStyles from '../components/layout/BaseStyles';
 import NProgressStyles from '../styles/nprogress.styles';
 import initNProgress from '../lib/initNProgress';
-import initializeStore from '../client/reduxStore';
+import { wrapper } from '../client/reduxStore';
 
 initNProgress();
 
-class MyApp extends App {
-  render() {
-    const { Component, pageProps, store } = this.props;
+function MyApp({ Component, pageProps }) {
+  const { store, props } = wrapper.useWrappedStore(pageProps);
 
-    return (
-      <Provider store={store}>
-        <BaseStyles />
-        <NProgressStyles />
-        <Head>
-          <title>CodeScrobble ► Easily scrobble VINYL and CD to Last.fm</title>
-        </Head>
-        <Component {...pageProps} />
-      </Provider>
-    );
-  }
+  return (
+    <Provider store={store}>
+      <BaseStyles />
+      <NProgressStyles />
+      <Head>
+        <title>CodeScrobble ► Easily scrobble VINYL and CD to Last.fm</title>
+      </Head>
+      <Component {...props.pageProps} />
+    </Provider>
+  );
 }
 
-export default withRedux(initializeStore)(MyApp);
+export default MyApp;
